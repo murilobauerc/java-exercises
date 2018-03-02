a75f07fb9e61a7c4e89820a5c2ac09016712add5
Murilo <panchobauer@gmail.com>
2018-03-02 12:47:49 -0300

    contando de 1 até 2000

---
+++ ContadorUm.java
@@ -0,0 +1,10 @@
+package contadorum;
+
+public class ContadorUm {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 1; i <= 2000; i++) {
+            System.out.print(i + "\n");
+        }
+    }
+}
