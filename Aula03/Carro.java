94cbb2a8d49c78d9b843d33b0b5237337cf8845d
Murilo <panchobauer@gmail.com>
2018-03-23 09:21:17 -0300

    Carro class

--- Aula03/Carro.java
+++ Aula03/Carro.java
@@ -22,29 +22,25 @@ public class Carro {
             System.out.print("Pane no carro! Você não pode aumentar a velocidade de forma negativa." + "\n");
         }
         double limiteVelocidade = this.capacidadeMotor * 110;
-        if (this.capacidadeMotor == 1.0 && qtdVelocidade > limiteVelocidade) System.out.print("ERRO! Você não pode dirigir mais que o limite máximo de velocidade.\n");
-        if (this.capacidadeMotor == 1.6 && qtdVelocidade > limiteVelocidade) System.out.print("ERRO! Você não pode dirigir mais do que o limite máximo de velocidade.\n");
-        if (this.capacidadeMotor == 2.0 && qtdVelocidade > limiteVelocidade) System.out.print("ERRO! Você não pode dirigir mais que o limite máximo de velocidade.\n");
-        else{
-            double velocidadeNova = this.velocidade + qtdVelocidade;
-            this.velocidade = velocidadeNova;
+        if (this.velocidade + velocidade > limiteVelocidade) {
+            System.out.print("ERRO! Você não pode dirigir mais que o limite máximo de velocidade.\n");
+            return;
         }
+        this.velocidade += qtdVelocidade;
     }
     public void reduzirVelocidade(double qtdVelocidade){
-        if (qtdVelocidade < 0 ) {
-            System.out.print("Pane no carro! Você não pode reduzir a velocidade de forma negativa." + "\n");
-        }else{
-            double velocidadeNova = this.velocidade - qtdVelocidade;
-            if (velocidadeNova >= 60) { // PAREI AQUI
-                System.out.print("AIR-BAG ATIVADO! Você reduziu a velocidade de forma abrupta (+60km/h)");
-            }else{
-                this.velocidade = velocidadeNova;
-            }
-            if (this.velocidade < 0) {
-                System.out.print("Pane no sistema! Você reduziu a velocidade mais do que deveria." + "\n");
-            }
-        }
-    }
+        if ((velocidade < 0) || (velocidade > this.velocidade)){
+            System.out.println("Velocidade invalida");
+            return;
+	}
+	if (velocidade >= 60){
+            System.out.println("AIR-BAG disparado");
+            this.velocidade = 0;
+            return;
+	}	
+	this.velocidade -= qtdVelocidade;
+	}
+
     public double getVelocidade() {
         return velocidade;
     }
@@ -55,3 +51,4 @@ public class Carro {
         this.capacidadeMotor = capacidadeMotor;
     }
 }
+
